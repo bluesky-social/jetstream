e443ef8f6757e56ec1073c7917de4997337e949b
jcalabro <jamesrcalabro@gmail.com>
2026-08-12 22:18:46 -0400

    more touchips to the full-replay example

--- examples/full-replay/main.go
+++ examples/full-replay/main.go
@@ -31,7 +31,7 @@ func main() {
 	args.APIKey = os.Getenv("JETSTREAM_API_KEY")
 
 	if err := run(context.Background(), &args); err != nil {
-		slog.Error("an error occurred while running the jetstream example", "err", err)
+		slog.Error("an error occurred while running the jetstream full-network example", "err", err)
 		os.Exit(1)
 	}
 }
@@ -46,24 +46,26 @@ func run(ctx context.Context, args *CLIArgs) error {
 		opts = append(opts, jetstream.WithAPIToken(args.APIKey))
 	}
 
+	// Create a client connection pool
 	client, err := jetstream.Subscribe(args.Host, opts...)
 	if err != nil {
 		return fmt.Errorf("failed to subscribe: %w", err)
 	}
-	defer client.Close()
+	defer client.Close() // Don't forget to clean up!
 
 	// Print an event every so often
+	var lastEvent jetstream.Event
 	ticker := time.NewTicker(time.Second)
 	defer ticker.Stop()
 
-	var lastEvent jetstream.Event
 	for batch, err := range client.Events(ctx) {
 		if err != nil {
 			if errors.Is(err, jetstream.ErrFatal) {
 				return fmt.Errorf("received fatal error: %w", err)
 			}
 
-			// You should handle other errors gracefully
+			// You should handle other errors gracefully, depending
+			// on what your application needs
 			fmt.Printf("got an error: %v\n", err)
 			continue
 		}
@@ -81,22 +83,12 @@ func run(ctx context.Context, args *CLIArgs) error {
 		// Print events every so often just to show we're doing something
 		select {
 		case <-ticker.C:
-			if err := printLastEvent(lastEvent); err != nil {
-				return fmt.Errorf("failed to marshal event: %w", err)
+			if err := json.NewEncoder(os.Stdout).Encode(lastEvent); err != nil {
+				return fmt.Errorf("failed to json encode lastEvent: %w", err)
 			}
 		default:
 		}
 	}
 
-	return nil
-}
-
-func printLastEvent(event jetstream.Event) error {
-	buf, err := json.Marshal(event)
-	if err != nil {
-		return err
-	}
-
-	fmt.Printf("%s\n", buf)
 	return nil
 }
